cf1efa981fd7099abf66ed863f6402adef90bfe3
Chad Stabler <stablerpsn@gmail.com>
2022-08-11 01:24:00 -0500

    Working route for Oauth

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+drop table if exists gh_users;
+
+create table gh_users(
+    id bigint generated always as identity primary key,
+    username text,
+    email text,
+    avatar text
+);
