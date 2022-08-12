65d9bba194b264240fec9b765e3cb3961f4e4ea6
Chad Stabler <stablerpsn@gmail.com>
2022-08-11 19:16:02 -0500

    sign out route tested and working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const agent = request.agent(app);
 
 jest.mock('../lib/services/github');
 
@@ -9,19 +10,11 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('should redirect to the github oauth page upon login', async () => {
-    const res = await request(app).get('/api/v1/github/login');
-
-    expect(res.header.location).toMatch(
-      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
-    );
-  });
   it('should login and redirect users to /api/v1/github/dashboard', async () => {
     const res = await request
       .agent(app)
       .get('/api/v1/github/callback?code=42')
       .redirects(1);
-    console.log(res);
 
     expect(res.body).toEqual({
       id: expect.any(String),
@@ -31,6 +24,10 @@ describe('backend-express-template routes', () => {
       iat: expect.any(Number),
       exp: expect.any(Number),
     });
+  });
+  it('should log out users', async () => {
+    const res = await agent.delete('/api/v1/github/callback?code=42');
+    expect(res.status).toBe(200);
   });
   afterAll(() => {
     pool.end();
